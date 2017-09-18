e1723b57db354107be26455a48a0579420732eab
ffreitas <ffreitas@ffreitas-macbook-pro.local>
2017-09-17 21:20:02 -0400

    esentials : first commit

---
+++ hello-world/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { EmailService } from './email.service';
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    CoursesService,
+    EmailService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
